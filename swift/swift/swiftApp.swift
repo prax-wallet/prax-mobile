f4a5d53844150171a36a46493d848f74d65a32ba
Tal Derei <talderei99@gmail.com>
2024-11-26 03:02:02 -0800

    swift scaffolding

---
+++ swift/swift/swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftApp.swift
+//  swift
+//
+//  Created by Tal Derei on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
