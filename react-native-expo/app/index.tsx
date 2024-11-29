a1233630ed6bf538ca615cb5538136f2520778dc
Tal Derei <talderei99@gmail.com>
2024-11-29 11:43:25 -0800

    expo module support

--- react-native-expo/app/index.tsx
+++ react-native-expo/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import MyModule from '@/modules/my-module';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>{MyModule.hello()}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  text: {
+    fontSize: 18,
+    color: '#333', 
+    fontWeight: 'bold',
+  },
+});
