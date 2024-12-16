c2e446c2078158d9dec99e573655dd63781a0764
Tal Derei <talderei99@gmail.com>
2024-12-15 16:54:12 -0800

    modify rust lib to mutex state

--- uniffi/src/lib.rs
+++ uniffi/src/lib.rs
@@ -2,127 +2,131 @@
 
 mod view_server;
 use anyhow::Result;
-use async_std::future::{pending, timeout};
 use once_cell::sync::OnceCell;
+use penumbra_keys::keys::SpendKey;
+use penumbra_keys::{test_keys, FullViewingKey};
 use penumbra_proto::view::v1::view_service_server::ViewServiceServer;
-use std::sync::Arc;
-use tokio::task;
-use tokio::{runtime::Runtime, sync::Mutex, time::Duration};
-use warp::Filter;
 use penumbra_view::ViewServer;
+use std::ops::Deref;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 uniffi::setup_scaffolding!();
 
-// Shared state for the counter
-static COUNTER: OnceCell<Arc<Mutex<u64>>> = OnceCell::new();
-
-#[uniffi::export]
-fn invoke_rust() -> String {
-    "Hello from Rust!".to_string()
-}
-
-#[uniffi::export(async_runtime = "tokio")]
-pub async fn perform_async_task(input: String) -> String {
-    let result = task::spawn(async move {
-        tokio::time::sleep(Duration::from_secs(2)).await;
-        format!("Processed: {input}")
-    })
-    .await;
-
-    result.unwrap_or_else(|_| "Task failed".to_string())
-}
-
-#[uniffi::export(async_runtime = "tokio")]
-pub async fn start_counter() -> bool {
-    let counter: &Arc<Mutex<u64>> = COUNTER.get_or_init(|| Arc::new(Mutex::new(0)));
-    let counter_clone = counter.clone();
-
-    task::spawn(async move {
-        loop {
-            {
-                let mut value = counter_clone.lock().await;
-                *value += 1;
-                println!("Counter incremented: {}", *value);
-            }
-            tokio::time::sleep(Duration::from_secs(2)).await;
-        }
-    });
-
-    true
-}
-
-#[uniffi::export(async_runtime = "tokio")]
-pub async fn get_counter() -> u64 {
-    let counter = COUNTER.get_or_init(|| Arc::new(Mutex::new(0)));
-    let value = counter.lock().await;
-    *value
-}
-
-#[uniffi::export]
-pub async fn say_after(ms: u64, who: String) -> String {
-    let never = pending::<()>();
-    timeout(std::time::Duration::from_millis(ms), never)
-        .await
-        .unwrap_err();
-    format!("Hello, {who}!")
-}
+// Global state
+static APP_STATE: OnceCell<Arc<AppStateContainer>> = OnceCell::new();
 
 const ENDPOINT: &str = "https://testnet.plinfra.net";
 
-pub async fn start_test_view_server() -> Result<ViewServer> {
-    ViewServer::load_or_initialize(
+#[derive(uniffi::Object)]
+struct AppState {
+    view_server: Option<Arc<ViewServer>>,
+    spend_key: Option<Arc<SpendKey>>,
+    full_viewing_key: Option<Arc<FullViewingKey>>,
+}
+
+// Wrapper type that uniffi can work with
+#[derive(uniffi::Object)]
+pub struct AppStateContainer {
+    state: Arc<Mutex<AppState>>,
+}
+
+#[derive(Debug, thiserror::Error, uniffi::Error)]
+pub enum AppError {
+    #[error("{0}")]
+    General(String),
+    #[error("View server error: {0}")]
+    ViewServer(String),
+}
+
+// Create initialize app state
+fn init_app_state() -> Mutex<AppState> {
+    Mutex::new(AppState {
+        view_server: None,
+        spend_key: None,
+        full_viewing_key: None,
+    })
+}
+
+#[uniffi::export]
+pub async fn create_app_state_container() -> Result<bool, AppError> {
+    let container = Arc::new(AppStateContainer {
+        state: Arc::new(init_app_state()),
+    });
+
+    APP_STATE
+        .set(container)
+        .map_err(|_| AppError::General("App state already initialized".to_string()))?;
+
+    Ok(true)
+}
+
+#[uniffi::export(async_runtime = "tokio")]
+pub async fn start_server() -> Result<bool, AppError> {
+    let state = APP_STATE
+        .get()
+        .ok_or("App state not initialized")
+        .expect("starting server");
+
+    let view_server = match ViewServer::load_or_initialize(
         None::<&str>,
         None::<&str>,
         &penumbra_keys::test_keys::FULL_VIEWING_KEY,
         ENDPOINT.parse().unwrap(),
     )
     .await
-}
-
-#[uniffi::export(async_runtime = "tokio")]
-pub async fn start_server() -> Option<u64> {
-    tracing::info!("Starting servers...");
-
-    // Start the test view server and handle any errors upfront.
-    let vs = match start_test_view_server().await {
-        Ok(vs) => vs,
-        Err(e) => {
-            eprintln!("Error starting view server: {}", e);
-            return None;
-        }
+    {
+        Ok(vs) => Arc::new(vs),
+        Err(e) => return Err(AppError::ViewServer(e.to_string())),
     };
 
-    // Get the block height before spawning tasks.
-    let block_height = match vs.latest_known_block_height().await {
-        Ok(height) => {
-            println!("Block height: {}", height.0);
-            height.0
-        }
-        Err(e) => {
-            eprintln!("Error querying block height: {}", e);
-            return None;
-        }
-    };
+    // Set dummy keys in state
+    let mut state = state.state.lock().await;
+    state.spend_key = Some(test_keys::SPEND_KEY.clone().into());
+    state.full_viewing_key = Some(test_keys::FULL_VIEWING_KEY.clone().into());
+    state.view_server = Some(view_server.clone());
 
-    // Spawn the Tonic server in the background.
     tokio::spawn(async move {
-        let vs_tonic = ViewServiceServer::new(vs);
+        let vs_tonic = ViewServiceServer::from_arc(view_server.clone());
         let server = tonic::transport::Server::builder()
             .accept_http1(true)
             .add_service(tonic_web::enable(vs_tonic))
             .serve("127.0.0.1:3333".parse().unwrap());
 
         if let Err(e) = server.await {
-            eprintln!("Error running tonic server: {}", e);
+            Err(format!("Error running tonic server: {}", e))
+        } else {
+            Ok(())
         }
     });
 
-    // Spawn the Warp server in the background.
-    tokio::spawn(async {
-        let routes = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-        warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-    });
-
-    println!("Servers started successfully");
-    Some(block_height)
+    Ok(true)
 }
 
+#[uniffi::export(async_runtime = "tokio")]
+pub async fn get_block_height() -> Result<u64, AppError> {
+    let state = APP_STATE
+        .get()
+        .ok_or("App state not initialized")
+        .expect("retrieving state");
+
+    let view_server = match &state.state.lock().await.view_server {
+        Some(vs) => vs.deref().clone(),
+        None => {
+            return Err(AppError::ViewServer(
+                "acquiring lock on view server".to_string(),
+            ));
+        }
+    };
+
+    let block_height = match view_server.latest_known_block_height().await {
+        Ok(height) => {
+            println!("Block height: {}", height.0);
+            height.0
+        }
+        Err(e) => {
+            return Err(AppError::ViewServer(e.to_string()));
+        }
+    };
+
+    Ok(block_height)
+}
