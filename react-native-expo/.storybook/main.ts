22b7a893063c6501a33db6dcbbc5f4d3dd8740c1
Jesse Pinho <jesse@jessepinho.com>
2024-12-19 13:55:02 -0800

    Remove Storybook onboarding addon

--- react-native-expo/.storybook/main.ts
+++ react-native-expo/.storybook/main.ts
@@ -41,7 +41,6 @@ const config: StorybookConfig = {
     });
   },
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
