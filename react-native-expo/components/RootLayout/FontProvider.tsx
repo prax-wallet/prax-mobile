d545ccec979afc2f9ecf24fab72c55b18b651cc2
Jesse Pinho <jesse@jessepinho.com>
2024-12-20 12:52:18 -0800

    Move fonts to assets dir; fix their use in Storybook

--- react-native-expo/components/RootLayout/FontProvider.tsx
+++ react-native-expo/components/RootLayout/FontProvider.tsx
@@ -8,12 +8,14 @@ interface FontsProps {
 
 export default function FontProvider({ children }: FontsProps) {
   const [loaded] = useFonts({
-    [dripsyTheme.customFonts.Poppins.default]: require('./fonts/Poppins-Regular.ttf'),
-    [dripsyTheme.customFonts.Poppins.italic]: require('./fonts/Poppins-Italic.ttf'),
-    [dripsyTheme.customFonts.Poppins.bold]: require('./fonts/Poppins-Medium.ttf'),
-    [dripsyTheme.customFonts.Poppins.boldItalic]: require('./fonts/Poppins-MediumItalic.ttf'),
-    [dripsyTheme.customFonts!.IosevkaTerm.default]: require('./fonts/IosevkaTerm-Regular.ttf'),
-    [dripsyTheme.customFonts!.IosevkaTerm.bold]: require('./fonts/IosevkaTerm-Medium.ttf'),
+    [dripsyTheme.customFonts.Poppins.default]: require('@/assets/fonts/Poppins-Regular.ttf'),
+    [dripsyTheme.customFonts.Poppins.italic]: require('@/assets/fonts/Poppins-Italic.ttf'),
+    [dripsyTheme.customFonts.Poppins.bold]: require('@/assets/fonts/Poppins-Medium.ttf'),
+    [dripsyTheme.customFonts.Poppins
+      .boldItalic]: require('@/assets/fonts/Poppins-MediumItalic.ttf'),
+    [dripsyTheme.customFonts!.IosevkaTerm
+      .default]: require('@/assets/fonts/IosevkaTerm-Regular.ttf'),
+    [dripsyTheme.customFonts!.IosevkaTerm.bold]: require('@/assets/fonts/IosevkaTerm-Medium.ttf'),
   });
 
   if (!loaded) return null;
