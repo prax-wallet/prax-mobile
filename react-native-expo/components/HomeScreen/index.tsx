fc957c1261b46a907867d6ccb2a4fa7374a39f93
Jesse Pinho <jesse@jessepinho.com>
2024-12-23 10:35:24 -0800

    Start building home screen UI

--- react-native-expo/components/HomeScreen/index.tsx
+++ react-native-expo/components/HomeScreen/index.tsx
@@ -1,7 +1,44 @@
-import { Text } from 'dripsy';
+import { Sx, Text, View } from 'dripsy';
 
 export interface HomeScreenProps {}
 
 export default function HomeScreen() {
-  return <Text>HomeRoute</Text>;
+  return (
+    <View sx={sx.root}>
+      <View sx={sx.balanceWrapper}>
+        <Text sx={sx.balanceLabel}>Balance</Text>
+        <Text sx={sx.balance}>0.00 USDC</Text>
+      </View>
+
+      <View sx={sx.buttons}></View>
+    </View>
+  );
 }
+
+const sx = {
+  balance: {
+    variant: 'text.h4',
+  },
+
+  balanceLabel: {
+    variant: 'text.small',
+
+    color: 'neutralLight',
+  },
+
+  balanceWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  buttons: {
+    flexDirection: 'row',
+  },
+
+  root: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+} satisfies Record<string, Sx>;
