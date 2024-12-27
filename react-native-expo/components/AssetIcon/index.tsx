0fa7b31af5ce1431debead709aef35b1d1024991
Jesse Pinho <jesse@jessepinho.com>
2024-12-26 20:50:00 -0800

    Add docblock

--- react-native-expo/components/AssetIcon/index.tsx
+++ react-native-expo/components/AssetIcon/index.tsx
@@ -1,5 +1,9 @@
 import { Sx, View } from 'dripsy';
 
+/**
+ * A dummy component that, once populated with data, will render an icon for a
+ * specific asset.
+ */
 export default function AssetIcon() {
   return <View sx={sx.root} />;
 }
