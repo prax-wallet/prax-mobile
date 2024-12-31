9f05197f3774be0f4ac65f6e39e594b62f2e679c
Jesse Pinho <jesse@jessepinho.com>
2024-12-31 10:11:49 -0800

    Add comment

--- react-native-expo/components/TransactionsScreen/useFilteredTransactions.ts
+++ react-native-expo/components/TransactionsScreen/useFilteredTransactions.ts
@@ -2,6 +2,13 @@ import { useAppSelector } from '@/store/hooks';
 import { useMemo } from 'react';
 import transactionSearchTextFilter from './useFilteredTransactions/transactionSearchTextFilter';
 
+/**
+ * Returns the transactions from state, filtered by the search text from state.
+ *
+ * The filtered results are memoized for performance reasons. In the future, if
+ * typing search text becomes slow, consider adding throttling updates to
+ * `filteredTransactions`.
+ */
 export default function useFilteredTransactions() {
   const transactions = useAppSelector(state => state.transactions.transactions);
   const searchText = useAppSelector(state => state.transactions.searchText);
