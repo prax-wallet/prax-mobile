1b6f291d4bebdcdb84f69ca62a49abf230eb14fd
Jesse Pinho <jesse@jessepinho.com>
2024-12-31 19:40:02 -0800

    Add comment

--- react-native-expo/components/PortfolioScreen/BalanceList/AssetActionSheet/useTransactionsForAsset.ts
+++ react-native-expo/components/PortfolioScreen/BalanceList/AssetActionSheet/useTransactionsForAsset.ts
@@ -1,7 +1,11 @@
 import { useAppSelector } from '@/store/hooks';
 import { useMemo } from 'react';
 
-export default function useTransactionsForAsset(assetSymbol?: string) {
+/** Returns a memoized array of the transactions for a given asset. */
+export default function useTransactionsForAsset(
+  /** The symbol of the asset that transactions should be filtered by. */
+  assetSymbol?: string,
+) {
   const transactions = useAppSelector(state => state.transactions.transactions);
 
   const transactionsForAsset = useMemo(
