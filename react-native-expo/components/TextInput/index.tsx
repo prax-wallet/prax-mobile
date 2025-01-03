897f38bca0aa37adaf1a55d8ce80403bb3a688c1
Jesse Pinho <jesse@jessepinho.com>
2025-01-02 18:54:20 -0800

    Add startAdornment prop; add stories

--- react-native-expo/components/TextInput/index.tsx
+++ react-native-expo/components/TextInput/index.tsx
@@ -1,17 +1,27 @@
 import { TextInput as DripsyTextInput, Sx, View } from 'dripsy';
+import { ReactNode } from 'react';
 
 export interface TextInputProps {
   value?: string;
   onChangeText?: (value: string) => void;
+  /**
+   * Markup to render to the left of the input field. For example, if you're
+   * using `TextInput` for a username field, you may wish to pass `'@'` into the
+   * `startAdornment` prop to show an `@` symbol to the left of the username
+   * field.
+   */
+  startAdornment?: ReactNode;
 }
 
 /**
  * Wraps React Native's `<TextInput />`, while adding Prax styles.
  */
-export default function TextInput({ value, onChangeText }: TextInputProps) {
+export default function TextInput({ value, onChangeText, startAdornment }: TextInputProps) {
   return (
     <View sx={sx.root}>
-      <DripsyTextInput value={value} onChangeText={onChangeText} />
+      {startAdornment}
+
+      <DripsyTextInput sx={sx.textInput} value={value} onChangeText={onChangeText} />
     </View>
   );
 }
@@ -24,5 +34,14 @@ const sx = {
 
     px: '$4',
     py: '$2',
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '$2',
+  },
+
+  textInput: {
+    appearance: 'none',
+    flexGrow: 1,
   },
 } satisfies Record<string, Sx>;
