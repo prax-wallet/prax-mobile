a5a2f1ee3fbb2ed91ddae4c4d186c242ed1b62c0
Jesse Pinho <jesse@jessepinho.com>
2025-01-02 16:40:55 -0800

    Add secureStore key to Redux

--- react-native-expo/store/index.ts
+++ react-native-expo/store/index.ts
@@ -1,20 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
+import createSecureStore from 'redux-persist-expo-securestore';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from 'redux-persist';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import balances from './balances';
 import depositFlow from './depositFlow';
 import portfolioScreen from './portfolioScreen';
+import secureStore from './secureStore';
 import transactions from './transactions';
 
+const rootReducer = combineReducers({
+  balances,
+  depositFlow,
+  portfolioScreen,
+  secureStore,
+  transactions,
+});
+
 const store = configureStore({
-  reducer: {
-    balances,
-    depositFlow,
-    portfolioScreen,
-    transactions,
-  },
+  reducer: persistReducer(
+    {
+      key: 'secureStore',
+      storage: createSecureStore(),
+    },
+    rootReducer,
+  ),
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          /**
+           * @see https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+           */
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+        ],
+      },
+    }),
 });
 
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
