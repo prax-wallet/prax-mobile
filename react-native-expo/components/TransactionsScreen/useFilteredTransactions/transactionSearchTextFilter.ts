35f28b0cfbc585fb16feb11ff881646aefc75638
Jesse Pinho <jesse@jessepinho.com>
2025-01-02 19:11:14 -0800

    Early return

--- react-native-expo/components/TransactionsScreen/useFilteredTransactions/transactionSearchTextFilter.ts
+++ react-native-expo/components/TransactionsScreen/useFilteredTransactions/transactionSearchTextFilter.ts
@@ -9,20 +9,19 @@ export default function transactionSearchTextFilter(searchText: string) {
 
   return (transaction: Transaction): boolean => {
     const memoLowerCase = transaction.memo?.toLocaleLowerCase();
+    if (memoLowerCase?.includes(searchTextLowerCase)) return true;
 
     switch (transaction.type) {
       case 'receive':
         return !!(
           transaction.senderAddress.toLocaleLowerCase().includes(searchTextLowerCase) ||
-          transaction.senderUsername?.includes(searchTextLowerCase) ||
-          memoLowerCase?.includes(searchTextLowerCase)
+          transaction.senderUsername?.includes(searchTextLowerCase)
         );
 
       case 'send':
         return !!(
           transaction.recipientAddress.toLocaleLowerCase().includes(searchTextLowerCase) ||
-          transaction.recipientUsername?.toLocaleLowerCase().includes(searchTextLowerCase) ||
-          memoLowerCase?.includes(searchTextLowerCase)
+          transaction.recipientUsername?.toLocaleLowerCase().includes(searchTextLowerCase)
         );
 
       default:
