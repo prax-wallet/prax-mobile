32263cdd41accea8f36ecb3dc2d8cfff240f6b73
Jesse Pinho <jesse@jessepinho.com>
2025-01-02 16:45:48 -0800

    Add persistor to the app

--- react-native-expo/components/ReduxProvider/index.tsx
+++ react-native-expo/components/ReduxProvider/index.tsx
@@ -1,11 +1,21 @@
 import store from '@/store';
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
 
 export interface ReduxProviderProps {
   children: ReactNode;
 }
 
 export default function ReduxProvider({ children }: ReduxProviderProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
